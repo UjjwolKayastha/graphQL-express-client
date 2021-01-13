8c08a14ba98267a032db8f6d041b549ed273567c
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-13 14:44:31 +0545

    feat: add browser router and switch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import { Route, Switch } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_API,
@@ -11,7 +15,12 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Home />
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
     </ApolloProvider>
   );
 };
