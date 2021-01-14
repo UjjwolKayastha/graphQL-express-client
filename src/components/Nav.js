97fa1cfdf5f793f747e3d55a29692400bb70b5e2
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-14 20:14:45 +0545

    feat: conditional render login register button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "firebase";
 import { AuthContext } from "../context/authContext";
@@ -35,16 +35,20 @@ const Nav = () => {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
+          {!user && (
+            <Fragment>
+              <li className="nav-item active">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+            </Fragment>
+          )}
           {user && (
             <li className="nav-item">
               <a onClick={logout} href="/login" className="nav-link">
