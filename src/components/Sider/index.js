66485af62aad846eaf8000973b269086e4cb5589
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-21 15:02:07 +0545

    feat: create sider component

---
+++ src/components/Sider/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sider = () => {
+  return (
+    <nav>
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <Link className="nav-link" to="/profile">
+            Profile
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/password/forgot">
+            Change Password
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/post/create">
+            Posts
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Sider;
