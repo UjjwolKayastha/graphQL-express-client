77676a11a84ae99f62b3e62ea6e4a22ab3ae12b8
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-25 21:25:24 +0545

    feat: add update password form

--- src/pages/auth/UpdatePassword.js
+++ src/pages/auth/UpdatePassword.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import AuthForm from "../../components/forms/AuthForm";
 
 const UpdatePassword = () => {
-  return <div>Update PASSWORD</div>;
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {};
+  return (
+    <div className="container p-5">
+      {loading ? (
+        <h4 className="text-info">Loading...</h4>
+      ) : (
+        <h4 className="text-primary">Continue Registration</h4>
+      )}
+      <AuthForm
+        password={password}
+        setPassword={setPassword}
+        loading={loading}
+        handleSubmit={handleSubmit}
+        buttonName={"Update"}
+        showPasswordInput
+        hideEmailInput
+      />
+    </div>
+  );
 };
 
 export default UpdatePassword;
