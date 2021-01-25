b898d62f065ca6e0069d575f0936949730a16c2c
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-25 21:28:19 +0545

    feat: update handleSubmit function for password update

--- src/pages/auth/UpdatePassword.js
+++ src/pages/auth/UpdatePassword.js
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import AuthForm from "../../components/forms/AuthForm";
+import { auth } from "../../firebase";
 
 const UpdatePassword = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      await auth.currentUser.updatePassword(password);
+      setPassword("");
+      setLoading(false);
+    } catch (e) {
+      setLoading(false);
+      console.log("Password update error", e);
+    }
+  };
   return (
     <div className="container p-5">
       {loading ? (
