8566417647bc07c68d1dbffa522dffb6bbcc4575
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-25 21:25:01 +0545

    feat: update email render props in auth form

--- src/components/forms/AuthForm.js
+++ src/components/forms/AuthForm.js
@@ -1,28 +1,31 @@
 import React from "react";
 
 const AuthForm = ({
-  email,
+  email = "",
   password = "",
   loading,
-  setEmail,
+  setEmail = (f) => f,
   setPassword,
   handleSubmit,
   showPasswordInput = false,
   buttonName,
+  hideEmailInput,
 }) => {
   return (
     <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Email</label>
-        <input
-          className="form-control"
-          type="email"
-          value={email}
-          placeholder="Enter email"
-          disabled={loading}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
+      {!hideEmailInput && (
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            className="form-control"
+            type="email"
+            value={email}
+            placeholder="Enter email"
+            disabled={loading}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+      )}
       {showPasswordInput && (
         <div className="form-group">
           <label>Password</label>
