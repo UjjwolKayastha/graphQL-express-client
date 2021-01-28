ca11703887feba2787e038fd9bbf80c908bb155e
Ujjwol Kayastha <uzol123@gmail.com>
2021-01-28 21:11:19 +0545

    feat: image upload and remove component

---
+++ src/components/FileUpload.js
@@ -0,0 +1,131 @@
+import React, { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { AuthContext } from "../context/authContext";
+import { toast } from "react-toastify";
+
+const FileUpload = ({ loading, values, setValues }) => {
+  const { state } = useContext(AuthContext);
+  const { images } = values;
+
+  console.log("IMAGES", values);
+  const fileResizeAndUpload = (event) => {
+    let fileInput = false;
+    if (event.target.files[0]) {
+      fileInput = true;
+    }
+    if (fileInput) {
+      try {
+        Resizer.imageFileResizer(
+          event.target.files[0],
+          300,
+          300,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            axios
+              .post(
+                `${process.env.REACT_APP_REST_API}/uploadimages`,
+                {
+                  image: uri,
+                },
+                {
+                  headers: {
+                    authtoken: state.user.token,
+                  },
+                }
+              )
+              .then((response) => {
+                setValues({
+                  ...values,
+                  images: [...images, response.data],
+                });
+                console.log(response);
+                toast.success("Image Uploaded successfully.");
+              })
+              .catch((error) => {
+                console.log("CLOUDINARY UPLOAD FAILED", error);
+              });
+          },
+          "base64",
+          200,
+          200
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  const handleImageRemove = (id) => {
+    axios
+      .post(
+        `${process.env.REACT_APP_REST_API}/removeimage`,
+        {
+          public_id: id,
+        },
+        {
+          headers: {
+            authtoken: state.user.token,
+          },
+        }
+      )
+      .then((response) => {
+        let filteredImages = images.filter((item) => {
+          return item.public_id !== id;
+        });
+        setValues({ ...values, images: filteredImages });
+        toast.info("Image successfully deleted");
+      })
+      .catch((error) => {
+        console.log("IMAGE REMOVE ERROR", error);
+      });
+  };
+
+  return (
+    <div className="row">
+      <div className="col-md-3">
+        <div className="form-group">
+          <label className="btn btn-primary">
+            Upload Image
+            <input
+              hidden
+              className="form-control"
+              disabled={loading}
+              type="file"
+              accept="image/*" //accepts file types with any extension
+              onChange={fileResizeAndUpload}
+            />
+          </label>
+        </div>
+      </div>
+      <div
+        className="col-md-9"
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        {images.map((image) => (
+          <div key={image.public_id} id="image-container">
+            <img
+              key={image.public_id}
+              height="100px"
+              src={image.url}
+              alt={image.public_id}
+              id="image"
+            />
+            <span
+              id="delete"
+              onClick={() => handleImageRemove(image.public_id)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FileUpload;
